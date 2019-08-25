ff40ea4884fca42a71f5285d77a885182fc6540b
yejianjie <yejianjie@fabu.ai>
2019-08-25 17:18:55 +0800

    2019/08/25

--- src/com/jenkinsye/oj/leetcode/LeetCode33.java
+++ src/com/jenkinsye/oj/leetcode/LeetCode33.java
@@ -1,8 +1,8 @@
 package com.jenkinsye.oj.leetcode;
 
-public class LeetCode81 {
-    public boolean search(int[] nums, int target) {
-        return binarySearch(nums, target, 0, nums.length - 1) > -1 ? true : false;
+public class LeetCode33 {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
     }
 
     public int binarySearch(int[] nums, int target, int low, int high) {
@@ -11,13 +11,11 @@ public class LeetCode81 {
         int mid = (low + high) / 2;
         if (nums[mid] == target)
             return mid;
-        if (nums[mid] == nums[high])
-            return binarySearch(nums, target, low, high - 1);
         if (nums[mid] < nums[high]) {
             if (nums[mid] < target && nums[high] >= target) {
                 return binarySearch(nums, target, mid + 1, high);
             } else
-                return binarySearch(nums, target, low, mid - 1);
+            return binarySearch(nums, target, low, mid - 1);
         }
         if (target >= nums[low] && target < nums[mid]) {
             return binarySearch(nums, target, low, mid - 1);
@@ -27,9 +25,8 @@ public class LeetCode81 {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 1, 3, 1};
-        int target = 3;
-        LeetCode81 leetCode81 = new LeetCode81();
-        System.out.println(leetCode81.search(nums, target));
+        int[] nums = new int[]{4, 5, 6,7,8,1,2,3};
+        int target = 8;
+        System.out.println(new LeetCode33().search(nums, target));
     }
 }
